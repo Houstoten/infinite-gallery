b5a6f58c947cc6c238d77eea28ea7bfdb0d7cf79
Ivan Husarov <ivan.husarov@gmail.com>
2022-05-29 23:49:22 +0300

    fixed untracked files

--- frontend/components/PublishDrawing.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-import { Button } from "@nextui-org/react";
-
-const PublishDrawing: FC<{ onPublishClick: () => void }> = ({ onPublishClick }) => {
-
-    return <Button onClick={onPublishClick}>Publish drawing</Button>
-}
-
-export default PublishDrawing
--- frontend/utils/toastUtils.ts
+++
@@ -1,3 +0,0 @@
-import toast from "react-hot-toast";
-
-export const warningToast = (text: string) => toast(text, { icon: "⚠️" })
